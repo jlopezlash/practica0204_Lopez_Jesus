6a4d7c772af1cb9080e99189860c6c13ea386b99
jlopezlash <jlopezlash1@educacion.navarra.es>
2024-10-29 13:21:44 +0100

    Create Ejercicio05.py

---
+++ Ejercicio05.py
@@ -0,0 +1,5 @@
+#Escribir un programa que almacene en una lista los números del 1 al 10 y
+#los muestre por pantalla en orden inverso separados por comas.
+Números = [1,2,3,4,5,6,7,8,9,10]
+Números.reverse()
+print(Números)
